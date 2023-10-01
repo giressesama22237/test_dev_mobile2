ff0767d35fe686bea4b877e1ba83c1ed34ca5d67
giressesama237 <gsiledje@gmail.com>
2023-10-01 15:30:39 +0100

    modif sur main

--- test3/src/main/kotlin/Main.kt
+++ test3/src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
     println("Hello World222222!")
     println("salut je suis la branche 2")
     println("salut je suis la branche 3")
-
+    println("salut je suis la main")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
